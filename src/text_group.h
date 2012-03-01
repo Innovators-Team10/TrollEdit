847f403e19a3556343dbb01122bc48339d3ddf4b
xtursky <xtursky@gmail.com>
2012-03-01 09:22:23 +0100

    [+] subory

---
+++ src/text_group.h
@@ -0,0 +1,41 @@
+/**
+ * block_group.h
+ *  ---------------------------------------------------------------------------
+ * Contains the declaration of class TextGroup and it's funtions and identifiers
+ *
+ */
+
+#ifndef TEXT_GROUP_H
+#define TEXT_GROUP_H
+
+#include <QObject>
+#include <QGraphicsTextItem>
+#include <QGraphicsSceneMouseEvent>
+
+class DocumentScene;
+class BlockGroup;
+
+class TextGroup : public QGraphicsTextItem
+{
+
+
+public:
+    TextGroup(BlockGroup *block, DocumentScene *scene);
+    ~TextGroup();
+
+    DocumentScene *scene;
+    BlockGroup *block;
+
+
+signals:
+
+public slots:
+
+protected:
+    void mousePressEvent(QGraphicsSceneMouseEvent *event);
+
+private:
+
+};
+
+#endif // TEXT_GROUP_H
