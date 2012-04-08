52cfc39099f29cd1fbda5904a667e094ba408fe2
jozef_krajcovic <jozef.krajcovic89@gmail.com>
2012-04-08 20:53:05 +0200

    [+] new simple on-line help

--- src/main.cpp
+++ src/main.cpp
@@ -115,6 +115,7 @@ int main(int argc, char *argv[])
 
     //w.setStyleSheet();
     splashScreen.show();
+    w.resize(1220,600);
     w.show();
 
     QTimer::singleShot(2000, &splashScreen, SLOT(close()));
