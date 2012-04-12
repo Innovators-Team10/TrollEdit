cab2d9c6d8d4fdf865a31cc0279cbbd6511e4152
xtursky <xtursky@gmail.com>
2012-04-12 12:41:36 +0200

    uprava rozlisenia

--- src/main.cpp
+++ src/main.cpp
@@ -45,46 +45,6 @@ static int setstyle(lua_State *L) {
     return 0;                         /* number of results in LUA*/
 }
 
-static void stackDump (lua_State *L)
-{
-    int i;
-    int top = lua_gettop(L);
-
-    qDebug("-------STACK--------|");
-    for (i = 1; i <= top; i++) { /* repeat for each level */
-        int t = lua_type(L, i);
-        switch (t)
-        {
-            case LUA_TSTRING:
-            { /* strings */
-                qDebug("%d. string: '%s'\t|", i, lua_tostring(L, i));
-                break;
-            }
-            case LUA_TBOOLEAN:
-            { /* booleans */
-                qDebug("%d. %s\t|", i, lua_toboolean(L, i) ? "true" : "false");
-                break;
-            }
-            case LUA_TNUMBER:
-            { /* numbers */
-                qDebug("%d. numbers %g\t|", i, lua_tonumber(L, i));
-                break;
-            }
-            case LUA_TFUNCTION:
-            { /* numbers */
-                qDebug("%d. function %s\t|", i, lua_tostring(L, i) );
-                break;
-            }
-            default:
-            { /* other values*/
-            qDebug("%d. other %s\t|", i, lua_typename(L, t));
-            break;
-            }
-        }
-        // qDebug("--------------------|"); /* put a separator */
-    }
-    qDebug("");
-}
 
 
 int main(int argc, char *argv[])
@@ -125,7 +85,7 @@ int main(int argc, char *argv[])
 
     //w.setStyleSheet();
     splashScreen.show();
-    w.resize(1220,600);
+    w.resize(1024,768);
     w.setWindowIcon (QIcon(":/icon16"));
     w.show();
 
